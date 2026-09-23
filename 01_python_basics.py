c16bb7f39d111cffb969ad2dc1a015a4700cfb35
Kishore <akishore1092@gmail.com>
2026-09-23 15:04:24 +0530

    basic Python practice file

--- 01_python_basics.py
+++ 01_python_basics.py
@@ -1,1 +1,26 @@
+# Python Basics - My First Practice File
 
+# Variables
+name = "Kishore"
+age = 22
+
+print("Name:", name)
+print("Age:", age)
+
+# Basic calculation
+num1 = 10
+num2 = 5
+
+sum_result = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+
+print("Sum:", sum_result)
+print("Difference:", difference)
+print("Product:", product)
+
+# Simple condition
+if age >= 18:
+    print("I am an adult.")
+else:
+    print("I am a minor.")
